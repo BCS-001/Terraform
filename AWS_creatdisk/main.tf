17f70891ba0f7d4752d63c28bb917dce20f1d6f3
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2026-01-28 18:55:02 +0530

    Update main.tf

--- AWS_creatdisk/main.tf
+++ AWS_creatdisk/main.tf
@@ -9,4 +9,16 @@ resource "aws_ebs_volume" "example" {
 
   // Uncomment below if you want to create a snapshot of the volume
   // snapshot_id = var.snapshot_id
+tags = {
+    Name = "regdisk"
+    Email = "clouddevopsdl@businesscoresolutions.com"
+    Project = "symphony"
+    Purpose = "regression"
+    UseCase = "regression"
+    SID = "REG"
+    StartDate = "28012026"
+    EndDate = "28012026"
+    StreamLead = "mani@businesscoresolutions.com"
+    Team = "cloud/devops"
+  }
 }
